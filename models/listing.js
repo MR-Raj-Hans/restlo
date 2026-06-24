28620b2b6f9cdfa1619d6c4c41d751213798c8d9
MR-Raj-Hans <rajhans1501@gmail.com>
2026-06-24 10:21:38 +0530

    Updated project

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const Schema= mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
     title: {
@@ -7,13 +7,13 @@ const listingSchema = new Schema({
         required: true,
     },
     description: String,
-     image: {
+    image: {
         type: String,
         default:
-             "https://images.unsplash.com/photo-1532664189809-02133fee698d?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set : (v) =>
-            v=== ""
-                ? "https://images.unsplash.com/photo-1532664189809-02133fee698d?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        set: (v) =>
+            v === ""
+                ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
                 : v,
     },
     price: Number,
